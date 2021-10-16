ff759d48c7e1a91f7360f8ed418bac888982f9a0
arun-kadari <arunkumar413@gmail.com>
2021-10-16 19:36:36 +0530

    rewrote the calendar using hooks

---
+++ src/hooks/useCalendar.js
@@ -0,0 +1,205 @@
+import React, { useState, useEffect, useDebugValue } from "react";
+
+export function useCalendar(key) {
+  const [centuryYears, setCenturyYears] = useState([
+    1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000,
+  ]);
+  const [isLeapYear, setIsLeapYear] = useState(false);
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth());
+  const [selectedDate, setSelectedDate] = useState(
+    new Date("2021-10-05T06:56:08.984Z").getDate()
+  );
+  const [eventHours, setEventHours] = useState([]);
+  const [eventMinutes, setEventMinutes] = useState([]);
+  const [selectedDateString, setSelectedDateString] = useState(
+    new Date().toISOString()
+  );
+
+  useDebugValue(eventHours ? "hours" : "hours");
+
+  const [selectedYearEvents, setSelectedYearEvents] = useState([]);
+  const [selectedMonthEvents, setSelectedMonthEvents] = useState([]);
+  const [selectedDateEvents, setSelectedDateEvents] = useState([]);
+
+  const [view, setView] = useState("Day");
+
+  const [events, setEvents] = useState([
+    {
+      date: "2021-10-05T06:56:08.984Z",
+      location: "Zoom",
+      summary: "Zoom Meeting",
+      description: "Zoom meeting for new calendar App",
+      link: "",
+    },
+    {
+      date: "2021-10-05T08:56:08.984Z",
+      location: "Zoom",
+      summary: "Zoom Meeting",
+      description: "Zoom meeting for new calendar App",
+      link: "",
+    },
+
+    {
+      date: "2021-10-05T10:56:08.984Z",
+      location: "Zoom",
+      summary: "Zoom Meeting",
+      description: "Zoom meeting for new calendar App",
+      link: "",
+    },
+    {
+      date: "2021-11-05T10:56:08.984Z",
+      location: "Zoom",
+      summary: "November Event",
+      description: "Zoom meeting for new calendar App",
+      link: "",
+    },
+  ]);
+
+  const days = useState([
+    { name: "Sunday" },
+    { name: "Monday" },
+    { name: "Tuesday" },
+    { name: "Wednesday" },
+    { name: "Thursday" },
+    { name: "Friday" },
+    { name: "Saturday" },
+  ]);
+
+  const months = useState([
+    { name: "Janurary", totalDays: 31 },
+    { name: "February", totalDays: isLeapYear ? 29 : 28 },
+    { name: "March", totalDays: 31 },
+    { name: "April", totalDays: 30 },
+    { name: "May", totalDays: 31 },
+    { name: "June", totalDays: 30 },
+    { name: "July", totalDays: 31 },
+    { name: "August", totalDays: 31 },
+    { name: "September", totalDays: 30 },
+    { name: "October", totalDays: 31 },
+    { name: "November", totalDays: 30 },
+    { name: "December", totalDays: 31 },
+  ]);
+
+  useEffect(
+    function () {
+      if (selectedYear % 4 === 0) {
+        setIsLeapYear(true);
+      } else if (centuryYears.includes(selectedYear)) {
+        setIsLeapYear(true);
+      }
+    },
+    [selectedYear, centuryYears]
+  );
+
+  function changeYear(year) {
+    setSelectedYear(year);
+  }
+
+  function changeMonth(month) {
+    setSelectedMonth(month);
+  }
+  function changeDate(date) {
+    setSelectedDate(date);
+  }
+  function changeView(view) {
+    setView(view);
+  }
+
+  function getEventByHour(hour) {
+    let event = selectedDateEvents.filter(function (item, index) {
+      return new Date(item.date).getHours() === hour;
+    });
+    return event;
+  }
+
+  useEffect(
+    function () {
+      let selectedYearEvents = events.filter(function (item, index) {
+        return new Date(item.date).getFullYear() === selectedYear;
+      });
+      setSelectedYearEvents(selectedYearEvents);
+    },
+
+    [events, selectedYear]
+  );
+
+  useEffect(
+    function () {
+      let selectedMonthEvents = selectedYearEvents.filter(function (
+        item,
+        index
+      ) {
+        return new Date(item.date).getMonth() === selectedMonth;
+      });
+
+      setSelectedMonthEvents(selectedMonthEvents);
+    },
+    [events, selectedMonth, selectedYearEvents]
+  );
+
+  useEffect(
+    function () {
+      let selectedDateEvents = selectedMonthEvents.filter(function (
+        item,
+        index
+      ) {
+        return new Date(item.date).getDate() === selectedDate;
+      });
+
+      setSelectedDateEvents(selectedDateEvents);
+    },
+    [events, selectedMonthEvents, selectedDate]
+  );
+
+  useEffect(
+    function () {
+      let slectedDateHourEvents = selectedDateEvents.filter(function (
+        item,
+        index
+      ) {
+        return new Date(item.date).getHours() === selectedDate;
+      });
+    },
+    [events, [selectedDateEvents]]
+  );
+
+  useEffect(
+    function () {
+      let eventHours = selectedDateEvents.map(function (item, index) {
+        return new Date(item.date).getHours();
+      });
+
+      setEventHours(eventHours);
+    },
+    [events, selectedDateEvents]
+  );
+
+  useEffect(
+    function () {
+      let eventMinutes = selectedDateEvents.map(function (item, index) {
+        return new Date(item.date).getMinutes();
+      });
+
+      setEventMinutes(eventMinutes);
+    },
+    [events, selectedDateEvents]
+  );
+
+  return {
+    selectedMonth,
+    selectedDate,
+    selectedYear,
+    selectedDateEvents,
+    selectedMonthEvents,
+    selectedYearEvents,
+    eventHours,
+    eventMinutes,
+    changeView,
+    changeYear,
+    changeMonth,
+    changeDate,
+    getEventByHour,
+    view,
+  };
+}
--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,190 +1,163 @@
 import React, { usState, useEffect, cloneElement, useState } from "react";
+import { useCalendar } from "../hooks/useCalendar";
 
-const hourContainerStyle = {
-  border: "solid 0.5px gray",
-  minHeight: 60,
-};
+export function Calendar(props) {
+  const {
+    view,
+    changeDate,
+    changeMonth,
+    changeYear,
+    changeView,
+    eventHours,
+    eventMinutes,
+    selectedDate,
+    selectedMonth,
+    selectedYear,
+    selectedMonthEvents,
+    selectedDateEvents,
+    selectedYearEvents,
+    getEventByHour,
+  } = useCalendar();
 
-const eventStyle = {
-  backgroundColor: "var(--primary)",
-  color: "var(--text-lightest)",
-  fontSize: "0.8rem",
-  border: "none",
-  padding: 5,
-  marginRight: 5,
-  borderRadius: 5,
-};
+  const hourContainerStyle = {
+    height: 60,
+    border: "solid 0.5px gray",
+  };
 
-function hourLableElements() {
-  let labelElements = [];
+  const eventStyle = {
+    fontSize: "0.8rem",
+    backgroundColor: "blue",
+    display: "block",
+    color: "white",
+    padding: 2,
+    margin: 2,
+    borderRadius: 5,
+  };
 
-  for (let i = 0; i <= 24; i++) {
-    labelElements.push(
+  const hourElements = [...Array(24).keys()].map(function (hourItem, index) {
+    if (eventHours.includes(hourItem) && selectedDateEvents.length > 0) {
+      let eventItem = selectedDateEvents.filter(function (item, index) {
+        return new Date(item.date).getHours() === hourItem;
+      });
+
+      console.log("############### Event Item #################");
+      console.log(eventItem[0].summary);
+
+      return (
+        <div key={hourItem.toString()} id={hourItem} style={hourContainerStyle}>
+          <span className="event" style={eventStyle}>
+            {" "}
+            {eventItem[0].summary}
+          </span>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          key={hourItem.toString()}
+          id={hourItem}
+          style={hourContainerStyle}></div>
+      );
+    }
+  });
+
+  const timeElements = [...Array(24).keys()].map(function (item, index) {
+    return (
       <div
+        key={item.toString()}
         style={{
+          height: 60,
           border: "solid 0.5px gray",
-          minHeight: 60,
-          justifyContent: "center",
-          display: "grid",
-          gridTemplateColumns: "1fr",
-          alignItems: "flex-start",
-          textAlign: "center",
-          borderRight: "none",
-          borderLeft: "none",
         }}>
-        {" "}
-        {i > 12 ? i - 12 + "PM" : i + "AM"}{" "}
+        <span>
+          {" "}
+          {item <= 11
+            ? item + "AM"
+            : item === 12
+            ? item + "PM (Noon)"
+            : item - 12 + "PM"}{" "}
+        </span>
+      </div>
+    );
+  });
+
+  const handleView = (evt) => {
+    changeView(evt.target.value);
+  };
+
+  const handleMonth = (evt) => {
+    changeMonth(parseInt(evt.target.value));
+  };
+
+  function toolBar() {
+    return (
+      <div
+        className="calendar-tool-bar"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr 1fr",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+        <div>
+          <select value={selectedMonth.toString()} onChange={handleMonth}>
+            <option value="0">January</option>
+            <option value="1">February</option>
+            <option value="2">March</option>
+            <option value="3">April</option>
+            <option value="4">May</option>
+            <option value="5">June</option>
+            <option value="6">July</option>
+            <option value="7">August</option>
+            <option value="8">September</option>
+            <option value="9">October</option>
+            <option value="10">November</option>
+            <option value="11">December</option>
+          </select>
+        </div>
+        <div>
+          <select onChange={handleView}>
+            <option value="day">Day</option>
+            <option value="week">Week</option>
+            <option value="month"> Month</option>
+            <option value="year">Year</option>
+          </select>
+        </div>
+        <div>
+          <h4> {"<"} </h4>
+        </div>
+        <div>
+          <h4> {">"} </h4>
+        </div>
       </div>
     );
   }
-  return labelElements;
-}
-
-export function Calendar() {
-  const [eventItems, setEventItems] = useState([4, 8, 12, 16]);
-  const centuryYears = [1900, 2000, 2100, 2200, 2300, 2400, 2500];
-  const [thisYear, setThisYear] = useState(new Date().getFullYear());
-  const [isLeapYear, setIsLeapYear] = useState(false);
-  const [monthDefinitions, setMonthDefinitions] = useState([]);
-
-  useEffect(function () {
-    if (thisYear % 4 === 0) {
-      setIsLeapYear(true);
-    } else if (centuryYears.includes(thisYear)) {
-      setIsLeapYear(true);
-    }
-  }, []);
-
-  useEffect(
-    function () {
-      setMonthDefinitions(months);
-    },
-    [isLeapYear]
-  );
-
-  const months = [
-    { name: "Janurary", num: 0, totalDays: 31 },
-    { name: "February", num: 1, totalDays: isLeapYear ? 29 : 28 },
-    { name: "March", num: 2, totalDays: 31 },
-    { name: "April", num: 3, totalDays: 30 },
-    { name: "May", num: 4, totalDays: 31 },
-    { name: "June", num: 5, totalDays: 30 },
-    { name: "July", num: 6, totalDays: 31 },
-    { name: "August", num: 7, totalDays: 31 },
-    { name: "September", num: 8, totalDays: 30 },
-    { name: "October", num: 9, totalDays: 31 },
-    { name: "November", num: 10, totalDays: 30 },
-    { name: "December", num: 11, totalDays: 31 },
-  ];
-
-  const [events, setEvents] = useState([
-    {
-      date: new Date("2021-10-05T06:56:08.984Z"),
-      location: "Zoom",
-      summary: "Zoom Meeting",
-      description: "Zoom meeting for new calendar App",
-      link: "",
-    },
-    {
-      date: new Date("2021-10-05T08:56:08.984Z"),
-      location: "Zoom",
-      summary: "Zoom Meeting",
-      description: "Zoom meeting for new calendar App",
-      link: "",
-    },
-
-    {
-      date: new Date("2021-10-05T10:56:08.984Z"),
-      location: "Zoom",
-      summary: "Zoom Meeting",
-      description: "Zoom meeting for new calendar App",
-      link: "",
-    },
-  ]);
-
-  let eventYears = events.map(function (item, index) {
-    return item.date.getFullYear();
-  });
-
-  let eventMonths = events.map(function (item, index) {
-    return item.date.getMonth();
-  });
-
-  let eventDates = events.map(function (item, index) {
-    return item.date.getDate();
-  });
-
-  let eventHours = events.map(function (item, index) {
-    return item.date.getHours();
-  });
-
-  let eventMinutes = events.map(function (item, index) {
-    return item.date.getMinutes();
-  });
-
-  function hourElements() {
-    let items = [];
-    for (let i = 0; i <= 24; i++) {
-      if (eventItems.includes(i)) {
-        items.push(
-          <div style={hourContainerStyle}>
-            <div className="event" style={eventStyle}>
-              Meeting
-            </div>
-          </div>
-        );
-      } else {
-        items.push(<div style={hourContainerStyle}></div>);
-      }
-    }
-    return items;
-  }
-
-  function eventElements() {
-    return events.map(function (item, index) {
-      return (
-        <div className="event" style={eventStyle}>
-          Meeting
-        </div>
-      );
-    });
-  }
 
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "50px 1fr 1fr 1fr",
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr",
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-        {hourLableElements()}
-      </div>
+    <div>
+      {toolBar()}
 
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: "1fr",
+          gridTemplateColumns: "100px 1fr 1fr",
           justifyContent: "center",
           alignItems: "center",
+          textAlign: "center",
         }}>
-        {hourElements()}
-      </div>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr",
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-        {hourElements()}
+        <div
+          style={{
+            display: "grid",
+          }}>
+          {timeElements}
+        </div>
+
+        <div style={{ display: "grid", gridTemplateColumns: "1fr" }}>
+          {hourElements}
+        </div>
+        <div style={{ display: "grid", gridTemplateColumns: "1fr" }}>
+          {hourElements}
+        </div>
       </div>
     </div>
   );
